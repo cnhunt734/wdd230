400f223167e987cd3310a3d2fd78646529d3f149
cnhunt734 <hun18012@byui.edu>
2022-06-20 18:05:21 -0600

    620

---
+++ lesson09/js/prophets.js
@@ -0,0 +1,37 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const cards = document.querySelector('.cards');
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    const prophets = data['prophets'];
+    prophets.forEach(displayProphets);
+  });
+  
+  function displayProphets(prophet) {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let dob = document.createElement('p');
+    let pob = document.createElement('p');
+    let portrait = document.createElement('img');
+  
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    dob.textContent = `Date of Birth: ${prophet.birthdate}`;
+    pob.textContent = `Birth Place: ${prophet.birthplace}`;
+
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+    portrait.setAttribute('loading', 'lazy');
+  
+    card.appendChild(h2);
+    card.appendChild(dob);
+    card.appendChild(pob);
+    card.appendChild(portrait);
+
+    document.querySelector('div.cards').appendChild(card);
+  }
+
+
