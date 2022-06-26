9fcb8ff914d4928a7c7af19d99172008b286035b
cnhunt734 <hun18012@byui.edu>
2022-06-25 22:12:46 -0600

    625

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -13,3 +13,37 @@ if (temperature <= 50 && windspeed > 3) {
     document.querySelector('#windchill').textContent = Math.round(35.74 + (.6215 * temperature) - (35.75 * Math.pow(windspeed, .16)) +
         (.4275 * temperature * Math.pow(windspeed, .16)), 0) + '°F'
 }
+
+const currentTemp = document.querySelector('#temp');
+const weatherIcon = document.querySelector('.weather-icon');
+const captionDesc = document.querySelector('#weather-desc');
+const windSpeed = document.querySelector('#windspeed');
+
+const baseUrl= "https://api.openweathermap.org/data/2.5/weather";
+
+const cityName = "Rexburg";
+const stateCode = "ID";
+const countryCode = "US";
+const appid = "8af6001c6baede227c9b9e34f19bac44";
+const units = "imperial";
+
+const url = `${baseUrl}?q=${cityName},${stateCode},${countryCode}&appid=${appid}&units=${units}`
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    const temperature = Math.round(data.main.temp);
+    currentTemp.textContent = temperature;
+
+    const speed = data.wind.speed;
+    windSpeed.textContent = speed;
+
+    const baseImageUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const desc = data.weather[0].description;
+    
+
+    //set source to url above
+    weatherIcon.setAttribute('src', baseImageUrl);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+});
